d5a4a6088fbd969abc42ce80a9718f670eab869c
Pravinkumar Singh <pravinkumarosingh@gmail.com>
2024-08-18 17:07:56 +0530

    spring without boot added

--- spring-without-boot/src/main/java/org/example/App.java
+++ spring-without-boot/src/main/java/org/example/App.java
@@ -1,5 +1,8 @@
 package org.example;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
  * Hello world!
  *
@@ -8,6 +11,8 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        ApplicationContext context = new ClassPathXmlApplicationContext("spring.xml");
+//        Dev dev = context.getBean(Dev.class);
+//        dev.build();
     }
 }
---
+++ spring-without-boot/src/main/java/org/example/Dev.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Dev {
+
+    public Dev(){
+        System.out.println("Dev Constructor");
+    }
+
+    public void build(){
+        System.out.println("This is Dev Class");
+    }
+}
